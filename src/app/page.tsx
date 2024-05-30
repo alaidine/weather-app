f202cc5155d03769b0afff77e101d8c295f396e3
alaidine <aa.alimlinde@gmail.com>
2024-05-30 14:42:58 +0400

    display info about the weather of a city

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,29 @@ function Title({ children }: { children: React.ReactNode }) {
   );
 }
 
+function WeatherInfo({ data }: { data: any }) {
+  if (data.cod == 404) return <div>Location not found</div>;
+  return (
+    <div className="w-full max-w-5xl">
+      <div className="flex flex-col items-center justify-center">
+        <div className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+          {data.name}
+        </div>
+        <div className="mt-2 text-sm text-gray-500">
+          {data.weather[0].description}
+        </div>
+        <div className="mt-2 text-sm text-gray-500">{data.main.temp} °C</div>
+        <div className="mt-2 text-sm text-gray-500">
+          Wind: {data.wind.speed} m/s
+        </div>
+        <div className="mt-2 text-sm text-gray-500">
+          Humidity: {data.main.humidity}%
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function Home() {
   const [data, setData] = useState<any>(null);
   const [value, setValue] = useState<string>("London");
@@ -37,26 +60,25 @@ export default function Home() {
   if (!data) return <div>Loading...</div>;
 
   return (
-    <main className="h-screen">
-      <div className="flex flex-col items-center justify-center p-5">
+    <main className="h-screen flex flex-col items-center w-full">
+      <div className="flex flex-col items-center justify-center w-full p-5 border-b border-gray-200">
         <Title>Weather App</Title>
       </div>
-      <div className="flex flex-row items-center justify-center">
-        <div className="flex flex-col items-center justify-center w-full max-w-5xl">
+      <div className="flex flex-row items-center justify-center w-2/3 max-w-5xl">
+        <div className="flex flex-col items-center justify-center w-full">
           <Select onValueChange={setValue}>
             <SelectTrigger className="w-[180px]">
               <SelectValue placeholder="Location" />
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="London">Lodon</SelectItem>
-              <SelectItem value="Paris">Paris</SelectItem>
+              <SelectItem value="London">London</SelectItem>
+              <SelectItem value="Glasgow">Glasgow</SelectItem>
+              <SelectItem value="Birmingham">Birmingham</SelectItem>
             </SelectContent>
           </Select>
         </div>
 
-        <div className="flex flex-col items-center justify-center w-full max-w-5xl">
-          <div>{data.name}</div>
-        </div>
+        <WeatherInfo data={data} />
       </div>
     </main>
   );
